baedb608586b99080b0cded5dcb3b9f0ea203821
ViceRabbit <105757172+ViceRabbit@users.noreply.github.com>
2022-07-08 18:59:03 -0400

    Delete creds.py

--- creds.py
+++
@@ -1,3 +0,0 @@
-gkey = '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'
-cfapikey = '$2a$10$nsNs1QbC3SXKRPu0wCTTae5j3vkbDXSF5J0b3AKu3QmSSHkxLfdv.'
-cfapikey2 = '$2a$10$TUr3ca0Sjw0eFSLGluTb1er6Agwwsfp2aZx/ULfwk8M9w4AKwykl.'
